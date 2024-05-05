963bca154409a962301588ef2d7e44f0067e43b6
Arjun <arjun149133@gmail.com>
2024-05-05 23:38:25 +0530

    simulator

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hey you</Text>
+      <Text>hey you yeah i am into you!</Text>
       <StatusBar style="auto" />
     </View>
   );
